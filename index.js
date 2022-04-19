5312e22738cf185f46afaa8c6fdfcce595792f32
Emily <emilykiddell@gmail.com>
2022-04-19 16:34:18 +1000

    fixed code so generate password function would always include at least one of each character type

--- index.js
+++ index.js
@@ -2,60 +2,81 @@ window.addEventListener("load", () => {
   //start your code here
 
 //DOM elements
-const resultEl = document.getElementById('result');
-let passwordLength = document.getElementById('password-length');
-const submitBtn = document.getElementById('submit');
+let passwordLength = document.querySelector('#password-length');
+const submitBtn = document.querySelector('#submit');
+const passwordDisplay = document.querySelector('#password-display');
 
-let characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','I','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','{','|','}','~','0','1','2','3','4','5','6','7','8','9'];
+//object of generator functions
+const randomChar = {
+  lower: getLowercase,
+  upper: getUppercase,
+  number: getNumber,
+  symbol: getSymbol
+};
 
-//Generate Password Function
+//generate event listener
+submitBtn.addEventListener('click',() => {
 
-function generatePassword() {
-  const password = [];
-  passwordLength = parseInt(passwordLength.value);
- 
-  for (let i = 0; i < passwordLength; i ++) {
-    const item = characters[Math.floor(Math.random() * characters.length)];
-    password.push(item);
-    } 
-   return password.join('');
-  }
+  if(passwordLength.value <= 3) {
+    alert('minimum-length of 4 required')
+    return '';
+  };
 
-//Generate event listener
-submitBtn.addEventListener('click', () => {
-
-  if (passwordLength.value >= 4) {
-    resultEl.innerText = generatePassword();
-  } else {
-    alert('minimum of 4 required');
-  }
-});
+    passwordLength = parseInt(passwordLength.value);
+    const hasLower = true;
+    const hasUpper = true;
+    const hasSymbol = true;
+    const hasNumber = true;
   
+    passwordDisplay.innerText = generatePassword(hasLower, 
+      hasUpper, 
+      hasSymbol, 
+      hasNumber, 
+      passwordLength);
+  }
+)
+
+// generate password function
+function generatePassword(lower, upper, number, symbol, passwordLength) {
+  //1.init pw var
+  //2.loop over length
+  //3.call generator function for each type
+  //4.add final password to password variable and return
+  let passwordResult = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{lower}, {upper}, {number}, {symbol}];
+  
+  for (let i = 0; i < passwordLength; i += typesCount) {
+    typesArr.forEach(type => {
+      const charName = Object.keys(type)[0];
+     // console.log('charName: ', charName);
+
+      passwordResult += randomChar[charName]();
+    })
+  }
+
+  passwordDisplay.style.color = 'black';
+
+  return passwordResult;
+}
+
 // generator functions
-/*
 
-function getLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+function getLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(getLowerCase())
-
-function getUpperCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+function getUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-  console.log(getUpperCase());
-
+  
 function getNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)  
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);  
 }
-
-console.log(getNumber());
 
 function getSymbol() {
-  const symbols = '!#$%&()*+,-./:;<=>?@[\]^_{|}~'
+  const symbols = '!#$%&()*+,-./:;<=>?@[\\]^_{|}~'
   return symbols[Math.floor(Math.random() * symbols.length)]; 
 }
 
-console.log(getSymbol());
-*/
-})
+});
